53e7ed9aa6bc89f864a6b874aa0f28de357efe14
daniluna91 <danilunadj@gmail.com>
2025-12-13 18:22:06 +0100

    ed

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,7 +1,4 @@
-// .eslintrc.cjs
-
 module.exports = {
-  // Configuración de ESLint para un proyecto React/Vite
   root: true,
   env: { browser: true, es2020: true, node: true },
   extends: [
@@ -14,10 +11,10 @@ module.exports = {
   parser: "@typescript-eslint/parser",
   plugins: ["react-refresh", "react"],
   rules: {
-    // 🚨 LA SOLUCIÓN DEFINITIVA: Desactivar la regla que pide importar React
+    //Desactivar la regla que pide importar React
     "react/react-in-jsx-scope": "off",
     "react/jsx-uses-react": "off",
-    "@typescript-eslint/no-explicit-any": "off", // Opcional: Para trabajar rápido con 'any'
+    "@typescript-eslint/no-explicit-any": "off", //para trabajar rápido con 'any'
   },
   settings: {
     react: {
